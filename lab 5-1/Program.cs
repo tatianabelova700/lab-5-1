e59c57adb8aac006e181281167fe0cc129d2db37
Tatyana <Tatyana@DESKTOP-74DNP3C>
2021-12-14 21:23:21 +0300

    Добавьте файлы проекта.

---
+++ lab 5-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int S = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                array [i] = int.Parse(Console.ReadLine());
+                S+= array [i];
+            }
+            Console.WriteLine();
+            Console.WriteLine(S);
+            Console.ReadKey();
+        }
+    }
+}
